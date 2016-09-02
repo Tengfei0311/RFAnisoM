ec931467ff63d75027a62abafd2a0fcb41274df4
Dytue <yangyan1995@hotmail.com>
2016-09-02 16:23:44 +0800

    Update rfbin.m
    1.change interval length
    2.add transverse component

--- rfbin.m
+++ rfbin.m
@@ -1,14 +1,23 @@
-function [rfb,bazbin]=rfbin(rf,baz)
+function [rfrb,rftb,bazbin]=rfbin(rfr,rft,baz)
 %%bin rf in each 5 degree
 num=360/5;
 flag=0;
 for i=1:num
-    rfcount(i)=sum((baz<=(5*i))&(baz>=5*(i-1)));
+    if i==1
+        bazindex=(baz<=5)&(baz>=355);
+        rfcount(i)=sum(bazindex);
+    else
+        bazindex=(baz<=(5*i))&(baz>=(5*i-5));
+    rfcount(i)=sum(bazindex);
+    end
     if rfcount(i)~=0
         flag=flag+1;
-     bazbin(flag)=i*5/2;
-        
-    rfb(:,flag)=sum(rf(:,(baz<=(5*i))&(baz>=5*(i-1))),2)/rfcount(i);
+     bazbin(flag)=(i-1)*5;
+    
+     
+    rfrb(:,flag)=sum(rfr(:,bazindex),2)/rfcount(i);
+    rftb(:,flag)=sum(rft(:,bazindex),2)/rfcount(i);
     end
 end
 end
+
