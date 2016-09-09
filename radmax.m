f072f1682760d2c27d00a87e67e3e00a3981eece
Dytue <yangyan1995@hotmail.com>
2016-09-09 10:41:44 +0800

    add a section
    when input is not a rad system...

--- radmax.m
+++ radmax.m
@@ -4,6 +4,10 @@ function ircos=radmax(para,r,theta,phi,deltat)
 
 time=para.time;
 psint=para.psint;
+%judge the input theta is rad system or angle system
+if max(theta)>2*pi
+theta=theta/180*pi;
+end
 
 for j=1:length(theta)
             rj(:,j)=interp1(time,r(:,j),time-deltat/2*cos(2*(phi-theta(j))),'linear');
