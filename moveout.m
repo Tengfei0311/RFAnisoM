8f98b0c72f293afd6e10172e60c693e9f1f9d796
Young <yy951016@mail.ustc.edu.cn>
2016-12-20 14:53:37 +0800

    add a option to input moho depth

--- moveout.m
+++ moveout.m
@@ -1,4 +1,4 @@
-function moveouttime=moveout(evdelta,evdepth,stddelta,stddepth)
+function moveouttime=moveout(evdelta,evdepth,stddelta,stddepth,moho)
 
 %calculate ray parameter 
 commandev=['taup_time -mod ak135 -ph P -deg ',num2str(evdelta),' -h ',num2str(evdepth),' --rayp'];
@@ -11,7 +11,10 @@ stdp=str2double(stdp);
 stdp=stdp*360/2/pi/6371;
 
 %calculate ak135 model
+if nargin<5
 moho=40;
+end
+
 evtime=(moho-35)*(sqrt(4.48.^-2-evp.^2)-sqrt(8.04.^-2-evp.^2))+15*(sqrt(3.85.^-2-evp.^2)-sqrt(6.5.^-2-evp.^2))+20*(sqrt(3.46.^-2-evp.^2)-sqrt(5.8.^-2-evp.^2));
 stdtime=(moho-35)*(sqrt(4.48.^-2-stdp.^2)-sqrt(8.04.^-2-stdp.^2))+15*(sqrt(3.85.^-2-stdp.^2)-sqrt(6.5.^-2-stdp.^2))+20*(sqrt(3.46.^-2-stdp.^2)-sqrt(5.8.^-2-stdp.^2));
 moveouttime=evtime-stdtime;
